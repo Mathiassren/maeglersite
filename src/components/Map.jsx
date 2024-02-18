7aab184b821ad4c768760fd7ed26a4e5a90c7c03
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2024-02-18 21:03:37 +0100

    added map component for contactsite

---
+++ src/components/Map.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const MapComponent = () => {
+  const position = [55.64152, 12.08035];
+
+  return (
+    <div className="h-96 w-full">
+      {" "}
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        className="h-full w-full"
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapComponent;
